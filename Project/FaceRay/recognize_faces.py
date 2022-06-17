dbc8313d4bbb719407107a7a5841c20b460d7ed7
kamel402 <kamel1419@gmail.com>
2022-06-17 06:30:51 +0300

    submission

--- Project/FaceRay/recognize_faces.py
+++ Project/FaceRay/recognize_faces.py
@@ -1,62 +1,65 @@
 # import the necessary packages
 import face_recognition
 import argparse
+from camera import VideoCam
 import pickle
 import cv2
 import ray
+import imutils
+import time
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-e", "--encodings", required=True,
 	help="path to serialized db of facial encodings")
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
+ap.add_argument("-r", "--resize", type=int, default=480,
+	help="resize the image")
+ap.add_argument("-v", "--video", default=argparse.SUPPRESS, type=str,
+	help="path to the (optional) video file")
 ap.add_argument("-d", "--detection-method", type=str, default="cnn",
 	help="face detection model to use: either `hog` or `cnn`")
 args = vars(ap.parse_args())
 
-@ray.remote
-def recognize(image, database):
-	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+DATABASE = None
+
+def detection(image):
 	# detect the (x, y)-coordinates of the bounding boxes corresponding
 	# to each face in the input image, then compute the facial embeddings
 	# for each face
-	boxes = face_recognition.face_locations(rgb,
+	boxes = face_recognition.face_locations(image,
 		model=args["detection_method"])
-	encodings = face_recognition.face_encodings(rgb, boxes)
-	# initialize the list of names for each face detected
-	names = []
+	
+	encodings = face_recognition.face_encodings(image, boxes)
 
+	return boxes, encodings
 
-	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(database["encodings"],
-			encoding)
-		name = "Unknown"
+@ray.remote
+def recognize(encoding):
 
-			# check to see if we have found a match
-		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
-			for i in matchedIdxs:
-				name = database["names"][i]
-				counts[name] = counts.get(name, 0) + 1
-			# determine the recognized face with the largest number of
-			# votes (note: in the event of an unlikely tie Python will
-			# select first entry in the dictionary)
-			name = max(counts, key=counts.get)
-		
-		# update the list of names
-		names.append(name)
+	matches = face_recognition.compare_faces(DATABASE["encodings"],
+		encoding)
+	name = "Unknown"
 
-	return boxes, names
+		# check to see if we have found a match
+	if True in matches:
+		# find the indexes of all matched faces then initialize a
+		# dictionary to count the total number of times each face
+		# was matched
+		matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+		counts = {}
+		# loop over the matched indexes and maintain a count for
+		# each recognized face face
+		for i in matchedIdxs:
+			name = DATABASE["names"][i]
+			counts[name] = counts.get(name, 0) + 1
+		# determine the recognized face with the largest number of
+		# votes (note: in the event of an unlikely tie Python will
+		# select first entry in the dictionary)
+		name = max(counts, key=counts.get)
+	else:
+		return None
+
+	return name
 
 
 def draw(image, boxes, names):
@@ -72,26 +75,58 @@ def draw(image, boxes, names):
 
 if __name__ == '__main__':
     
-	# initialize ray
 	ray.init()
 
-    # load the known faces and embeddings
+	# load the known faces and embeddings
 	print("[INFO] loading encodings...")
-	database = pickle.loads(open(args["encodings"], "rb").read())
+	DATABASE = pickle.loads(open(args["encodings"], "rb").read())
 
-	# load the input image and convert it from BGR to RGB
-	image = cv2.imread(args["image"])
 
-	# recognize faces using ray remote function and return promises
-	print("[INFO] recognizing faces...")
-	futures = recognize.remote(image, database)
+	# Get the video
+	source = args["video"]
 
-	# get the bounding boxes and their names from the promises
-	boxes, names = ray.get(futures)
+	(H, W) = (None, None)
 
-	# draw the bounding boxes and names into the image
-	image = draw(image, boxes, names)
+	# Infernce 
+	cap = VideoCam(source)
+	cap.check_camera(cap.cap)
+	print("[INFO] Start recognizing")
 
-	# show the output image
-	cv2.imshow("Image", image)
-	cv2.waitKey(0)
+	ct = 0
+	start_time = time.time()
+
+	while True:
+		ct += 1
+		try:
+			ret = cap.cap.grab()
+			
+			ret, frame = cap.get_frame()
+			if not ret:
+				break
+
+			if W is None or H is None:
+				(H, W) = frame.shape[:2]
+			
+			frame = imutils.resize(frame, width=args["resize"])
+
+			rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+			boxes, encodings = detection(rgb)
+
+			names = []
+
+	
+			futures = [recognize.remote(encoding) for encoding in encodings]
+			names = ray.get(futures)
+
+			names = [i for i in names if i]
+
+			print("[INFO] Recognized {0} in Frame {1}".format(names, ct))
+
+		except KeyboardInterrupt:
+			cap.close_cam()
+			exit(0)
+
+	print('[INFO] This video took: '+str(time.time() - start_time)+' sec')
+
+	cap.close_cam()
+	exit(0)
